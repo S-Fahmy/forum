dbe7927befd881cf1ee1e1b06cc99bb5d4d9e8bc
sf <saleh.fahmy2@gmail.com>
2020-06-24 23:35:08 +0200

    5555

--- resources/js/components/Wysiwyg.vue
+++ resources/js/components/Wysiwyg.vue
@@ -100,8 +100,8 @@ export default {
 
           let url = response.data.url;
           var attributes = {
-            url: 'ist-industries.com/forum/' + url,
-            href:'ist-industries.com/forum/' + url + "?content-disposition=attachment"
+            url: 'http://ist-industries.com/forum/' + url,
+            href:'http://ist-industries.com/forum/' + url + "?content-disposition=attachment"
           };
           console.log(attributes.href);
           successCallback(attributes);
