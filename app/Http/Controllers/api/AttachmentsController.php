8b3e92a8d7b8861bc42e732704689e6f68595581
sf <saleh.fahmy2@gmail.com>
2020-06-24 22:48:27 +0200

    kos

--- app/Http/Controllers/api/AttachmentsController.php
+++ app/Http/Controllers/api/AttachmentsController.php
@@ -22,7 +22,7 @@ class AttachmentsController extends Controller
 
         return  Attachments::create([
 
-            'url' =>  Storage::url($path),
+            'url' => asset(Storage::url($path)),
             'user_id' => auth()->id(),
         ]);
     }
