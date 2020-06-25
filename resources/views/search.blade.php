d0fbfffb1a3fbd9db4d4817100bf2778748aade1
sf <saleh.fahmy2@gmail.com>
2020-06-25 16:09:12 +0200

    s

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
-<search :trending = "{{$trendingThreads}}"></search>
+{{-- :trending = "{{$trendingThreads}}" --}}
+<search ></search> 
 
  @endsection
